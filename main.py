822fea72dee2e081237feebffc7f543f5519fd7d
Flynn <flynnwebdev@gmail.com>
2024-12-02 19:59:36 +1000

    Lesson finished

---
+++ main.py
@@ -0,0 +1,4 @@
+from color50 import rgb, constants
+
+my_color = rgb(128, 0, 128)
+print(constants.YELLOW + "Hello, World!" + constants.RESET)
---
+++ modules/my_module.py
@@ -0,0 +1,12 @@
+greeting = 'Hello World'
+
+nums = [10, 20, 30]
+
+person = {
+    'name': 'John',
+    'age': 20
+}
+
+def foo(person):
+    print(f'{person.get('name')} is {person.get('age')} years old.')
+
